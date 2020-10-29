c6d83dad663fd4d4fe9fac37347274e5557e5c1c
lengswee@babyambrosia.com <lengswee@babyambrosia.com>
2020-10-29 20:28:52 +0800

    add window load listener

--- table-booking-app/scripts/book.js
+++ table-booking-app/scripts/book.js
@@ -18,17 +18,21 @@ function BookNow(guestName,guestEmail,guestPax){
     .then(json => {
       // Do something with object
       console.log(json.booking);
-      document.getElementById("bookMsg").innerHTML = json.booking.name + "successfully added!";
-      GetBookings();
+      //document.getElementById("bookMsg").innerHTML = json.booking.name + "successfully added!";
+      //GetBookings();
     });
     }
 
-    document.getElementById("bookNow").addEventListener("click", function(){
-        let name = document.getElementById("guestName").value;
-        let email = document.getElementById("guestEmail").value;
-        let pax = document.getElementById("guestPax").value;
-        let remark = document.getElementById("guestRemarks").value;
+    window.addEventListener("load",function(){
+        document.getElementById("bookNow").addEventListener("click", function(){
+            let name = document.getElementById("guestName").value;
+                let email = document.getElementById("guestEmail").value;
+                let pax = document.getElementById("guestPax").value;
+                let remark = document.getElementById("guestRemarks").value;
+        
+                console.log(name + "," + email + "," + pax + "," + remarks);
+        
+            });
+    });
 
-        alert(name + "," + email + "," + pax + "," + remarks);
-
-    })
+   
