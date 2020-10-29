78424f21a0f4b5cf87073b114aa97f7770945dcf
lengswee@babyambrosia.com <lengswee@babyambrosia.com>
2020-10-29 19:25:54 +0800

    added ids to book.html and events listener in book.js

---
+++ table-booking-app/scripts/book.js
@@ -0,0 +1,34 @@
+function BookNow(guestName,guestEmail,guestPax){
+    let url = 'https://api.sheety.co/ee581d1468ba79b9f91410ef97673070/bookingApp/booking';
+    let body = {
+      booking: {
+        name:guestName,
+        email:guestEmail,
+        pax:guestPax
+      }
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers:{
+        "Content-Type":"application/json"
+      }
+    })
+    .then((response) => response.json())
+    .then(json => {
+      // Do something with object
+      console.log(json.booking);
+      document.getElementById("bookMsg").innerHTML = json.booking.name + "successfully added!";
+      GetBookings();
+    });
+    }
+
+    document.getElementById("bookNow").addEventListener("click", function(){
+        let name = document.getElementById("guestName").value;
+        let email = document.getElementById("guestEmail").value;
+        let pax = document.getElementById("guestPax").value;
+        let remark = document.getElementById("guestRemarks").value;
+
+        alert(name + "," + email + "," + pax + "," + remarks);
+
+    })
